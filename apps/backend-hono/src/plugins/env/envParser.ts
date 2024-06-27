51ad7cfe266dad5884b11fcbf226c20c1e28c534
Ripwords <teohjjteoh@gmail.com>
2024-06-27 09:51:26 +0800

    remove hono

--- apps/backend-hono/src/plugins/env/envParser.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-
-export const envSchema = z.object({
-  SERVER_PORT: z.string().regex(/^\d+$/),
-  WEBSITE_URL: z.string(),
-  PASSWORD_PEPPER: z.string(),
-  DATABASE_URL: z.string(),
-})
